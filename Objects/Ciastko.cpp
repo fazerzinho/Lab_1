ddfce27794bf336f6f2eeaae1aa5104e8733b7aa
Wiktor Jóźwiak <45016808+fazerzinho@users.noreply.github.com>
2021-12-01 18:50:11 +0100

    z.2

--- Objects/Ciastko.cpp
+++ Objects/Ciastko.cpp
@@ -17,3 +17,35 @@ Ciastko::Ciastko(const string &rodzaj) : rodzaj(rodzaj) {
 Ciastko::~Ciastko() {
     cout << "Destruktor: " << this->rodzaj << "\n";cout << "Konstruktor domyślny: " << this->rodzaj << "\n";
 }
+
+const string &Ciastko::getRodzaj() const {
+    return rodzaj;
+}
+
+void Ciastko::setRodzaj(const string &rodzaj) {
+    Ciastko::rodzaj = rodzaj;
+}
+
+int Ciastko::getLicznikCiastek() {
+    return licznikCiastek;
+}
+
+void Ciastko::setLicznikCiastek(int licznikCiastek) {
+    Ciastko::licznikCiastek = licznikCiastek;
+}
+
+int Ciastko::getIdentyfikatorCiastka() {
+    return identyfikatorCiastka;
+}
+
+void Ciastko::setIdentyfikatorCiastka(int identyfikatorCiastka) {
+    Ciastko::identyfikatorCiastka = identyfikatorCiastka;
+}
+
+int Ciastko::getMójNumer() const {
+    return mójNumer;
+}
+
+void Ciastko::setMójNumer(int mójNumer) {
+    Ciastko::mójNumer = mójNumer;
+}
--- Objects/Ciastko.h
+++ Objects/Ciastko.h
@@ -10,14 +10,33 @@
 using namespace std;
 
 class Ciastko {
-private: string rodzaj;
-
+private:
+    string rodzaj;
+    static int licznikCiastek;  //id
+    static int identyfikatorCiastka;    //next objects id
+    int mójNumer; //id with prefix
 public:
     Ciastko();
 
     Ciastko(const string &rodzaj);
 
     virtual ~Ciastko();
+
+    const string &getRodzaj() const;
+
+    void setRodzaj(const string &rodzaj);
+
+    static int getLicznikCiastek();
+
+    static void setLicznikCiastek(int licznikCiastek);
+
+    static int getIdentyfikatorCiastka();
+
+    static void setIdentyfikatorCiastka(int identyfikatorCiastka);
+
+    int getMójNumer() const;
+
+    void setMójNumer(int mójNumer);
 };
 
 
