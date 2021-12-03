80d33f66dd3773fea9d1db077570c60288f01d90
Wiktor Jóźwiak <45016808+fazerzinho@users.noreply.github.com>
2021-12-03 16:55:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Objects/Ciastko.h"
 #include "list"
 int main() {
+    //do dodania: przyznawanie id, uzytkownik itd
     list<Ciastko> lista;
     lista.begin();
     lista.assign(1,Ciastko());
